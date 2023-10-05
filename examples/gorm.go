b26574b31dec97ea4a0a535a574cf162f05436af
kwkw <wei497896143@yeah.net>
2023-10-05 03:11:00 +0000

    chore: mysql port 33306 -> 3306

--- examples/gorm.go
+++ examples/gorm.go
@@ -19,7 +19,7 @@ func printMsg(j agscheduler.Job) {
 func main() {
 	agscheduler.RegisterFuncs(printMsg)
 
-	dsn := "root:123456@tcp(127.0.0.1:33306)/test?charset=utf8mb4&parseTime=True&loc=UTC"
+	dsn := "root:123456@tcp(127.0.0.1:3306)/test?charset=utf8mb4&parseTime=True&loc=UTC"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	// db, err := gorm.Open(sqlite.Open("agscheduler.db"), &gorm.Config{})
