9cc8d0d07152a8dc7f02b9373aa35d023b5b72b9
kwkw <wei497896143@yeah.net>
2024-01-24 00:50:41 +0000

    test(Cluster): update example

--- examples/cluster/cluster_node.go
+++ examples/cluster/cluster_node.go
@@ -1,16 +1,16 @@
 // Normal Mode
 //
-// 1. go run examples/cluster/cluster_main.go
-// 2. go run examples/cluster/cluster_node.go -e 127.0.0.1:36381 -eh 127.0.0.1:36391 -se 127.0.0.1:36361 -seh 127.0.0.1:36371
-// 3. go run examples/cluster/cluster_node.go -e 127.0.0.1:36382 -eh 127.0.0.1:36392 -se 127.0.0.1:36362 -seh 127.0.0.1:36372
+// 1. go run examples/cluster/cluster_node.go -e 127.0.0.1:36380
+// 2. go run examples/cluster/cluster_node.go -me 127.0.0.1:36380 -e 127.0.0.1:36381 -eh 127.0.0.1:36391 -se 127.0.0.1:36361 -seh 127.0.0.1:36371
+// 3. go run examples/cluster/cluster_node.go -me 127.0.0.1:36380 -e 127.0.0.1:36382 -eh 127.0.0.1:36392 -se 127.0.0.1:36362 -seh 127.0.0.1:36372
 // 4. go run examples/rpc/rpc_client.go
 
 // HA Mode
-// NOTE: All HA nodes need to connect to the same Store
+// NOTE: All HA nodes need to connect to the same Store (excluding `MemoryStore`)
 //
-// 1. go run examples/cluster/cluster_main.go -m HA
-// 2. go run examples/cluster/cluster_node.go -e 127.0.0.1:36381 -eh 127.0.0.1:36391 -se 127.0.0.1:36361 -seh 127.0.0.1:36371 -m HA
-// 3. go run examples/cluster/cluster_node.go -e 127.0.0.1:36382 -eh 127.0.0.1:36392 -se 127.0.0.1:36362 -seh 127.0.0.1:36372 -m HA
+// 1. go run examples/cluster/cluster_node.go -e 127.0.0.1:36380 -m HA
+// 2. go run examples/cluster/cluster_node.go -me 127.0.0.1:36380 -e 127.0.0.1:36381 -eh 127.0.0.1:36391 -se 127.0.0.1:36361 -seh 127.0.0.1:36371 -m HA
+// 3. go run examples/cluster/cluster_node.go -me 127.0.0.1:36380 -e 127.0.0.1:36382 -eh 127.0.0.1:36392 -se 127.0.0.1:36362 -seh 127.0.0.1:36372 -m HA
 // 4. go run examples/rpc/rpc_client.go
 
 package main
@@ -28,12 +28,12 @@ import (
 	"github.com/kwkwc/agscheduler/stores"
 )
 
-var mainEndpoint = flag.String("me", "127.0.0.1:36380", "Cluster Main Node endpoint")
-var endpoint = flag.String("e", "127.0.0.1:36381", "Cluster Node endpoint")
-var endpointHTTP = flag.String("eh", "127.0.0.1:36391", "Cluster Node endpoint HTTP")
-var schedulerEndpoint = flag.String("se", "127.0.0.1:36361", "Cluster Node Scheduler endpoint")
-var schedulerEndpointHTTP = flag.String("seh", "127.0.0.1:36371", "Cluster Main Node Scheduler endpoint HTTP")
-var queue = flag.String("q", "node", "Cluster Node queue")
+var mainEndpoint = flag.String("me", "", "Cluster Main Node endpoint")
+var endpoint = flag.String("e", "127.0.0.1:36380", "Cluster Node endpoint")
+var endpointHTTP = flag.String("eh", "127.0.0.1:36390", "Cluster Node endpoint HTTP")
+var schedulerEndpoint = flag.String("se", "127.0.0.1:36360", "Cluster Node Scheduler endpoint")
+var schedulerEndpointHTTP = flag.String("seh", "127.0.0.1:36370", "Cluster Node Scheduler endpoint HTTP")
+var queue = flag.String("q", "default", "Cluster Node queue")
 var mode = flag.String("m", "", "Cluster Node mode, options `HA`")
 
 func main() {
